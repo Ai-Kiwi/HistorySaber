e49bfae83e1a4948c816569e034f94fa0a5e866c
Ai-Kiwi <contact@aikiwi.dev>
2025-05-10 22:07:41 +1200

    made clickable area on score larger

--- src/lib/scoreDisplay.svelte
+++ src/lib/scoreDisplay.svelte
@@ -130,7 +130,7 @@ import type { Score } from "./types";
     }
 
     .placement-text {
-        margin-left: auto;
+        width: 100%;
         height: 100%;
         display: flex;
         flex-direction: column;
