e3f342140f5615d1dc7b34b5145d0d1851077902
Ai-Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2025-05-22 20:02:31 +1200

    added search system and onload load

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+    import Pagination from "$lib/pagination.svelte";
+    import type { UserType } from "$lib/types";
+    import Userbar from "$lib/userbar.svelte";
+    import UserBarLabels from "$lib/userBarLabels.svelte";
+    import { onMount } from "svelte";
+    import { flip } from "svelte/animate";
+
+  let loading_scores = $state(false)
+  let loading_outdated = false
+  let user_text_searching = $state("")
+  let user_page_selected = $state(1)
+  let search_users: UserType[]  = $state([])
+
+  async function fetch_search() {
+    if (loading_scores == true) {
+      loading_outdated = true
+      return
+    }
+    loading_scores = true;
+
+    try{
+      const params = new URLSearchParams({ 
+        page: user_page_selected.toString(), 
+        text: user_text_searching, 
+      });
+      const res = await fetch(`/api/search?${params.toString()}`);
+      if (!res.ok) {
+        console.error('Failed to fetch user search data:', res.statusText);
+        loading_scores = false;
+        return;
+      }
+
+      const user_data = await res.json();
+      search_users = user_data
+
+    }catch(e){
+      console.log(`failed fetching data ${e}`)
+    }
+
+
+    loading_scores = false
+    if (loading_outdated == true) {
+      loading_outdated = false
+      setTimeout(fetch_search, 100);
+    }
+  }
+
+  onMount(() => {
+    fetch_search()
+  })
+</script>
+
+<main>
+    <h1>Search</h1>
+
+    <input class="search-text" placeholder="username" bind:value={user_text_searching} on:input={fetch_search}>
+
+    <Pagination current_page_selected={user_page_selected} pageChanged={(page: number) => {user_page_selected = page; fetch_search()}}></Pagination>
+
+
+    <div class="{loading_scores ? 'shimmer' : ''}">
+        {#if search_users.length > 0}
+        <div class="search-list">
+        <UserBarLabels></UserBarLabels>
+          {#each search_users as user (user.player_id)}
+            <label animate:flip={{ duration: 500 }}>
+              <Userbar user={user}></Userbar>
+            </label>
+          {/each}
+        </div>
+      {:else}
+      <h2>
+        No search results
+      </h2>
+      {/if}
+      </div>
+
+
+      <Pagination current_page_selected={user_page_selected} pageChanged={(page: number) => {user_page_selected = page; fetch_search()}}></Pagination>
+
+</main>
+
+<style>
+    .search-list {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 40px;
+    }
+    h2 {
+        text-align: center;
+    }
+
+  .search-text {
+    background-color: black;
+    margin: 15px;
+    border-radius: 15px;
+    width: 100%;
+    border: 0px;
+    color: white;
+    padding: 5px;
+    font-size: 20px;
+  }
+
+
+  /*loading shimmer*/
+	@keyframes shimmer {
+		0% {
+			background-position: -200% 0;
+		}
+		100% {
+			background-position: 200% 0;
+		}
+	}
+
+	.shimmer {
+		position: relative;
+		overflow: hidden;
+    border-radius: 15px;
+	}
+
+	.shimmer::after {
+		content: "";
+		position: absolute;
+		inset: 0;
+		background: linear-gradient(
+			120deg,
+			rgba(50, 50, 50, 0) 0%,
+			rgba(255, 255, 255, 0.1) 50%,
+			rgba(50, 50, 50, 0) 100%
+		);
+		background-size: 200% 100%;
+		animation: shimmer 1.8s infinite linear;
+		pointer-events: none;
+		border-radius: inherit;
+	}
+</style>
