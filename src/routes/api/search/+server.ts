6dec35dc2aa89e56d273bdc22b8149a7926b46d9
Ai-Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2025-05-22 22:02:51 +1200

    increased search range up shouldn't have been that low

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -5,9 +5,9 @@ export async function GET({ url }) {
   try {
     const page = parseInt(url.searchParams.get('page') || '1');
     const text = url.searchParams.get('text') || '';
-    let page_size = Number(url.searchParams.get('page_size')) || 8;
+    let page_size = Number(url.searchParams.get('page_size')) || 50;
     if (page_size < 1 || page_size > 100) {
-      page_size = 8
+      page_size = 50
     }
 
     const users = await searchForUser(text,page,page_size)
