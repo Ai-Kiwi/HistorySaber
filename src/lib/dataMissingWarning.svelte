93508ce55af13ac64ba0c0dab27930eb6f994f19
Ai-Kiwi <contact@aikiwi.dev>
2025-07-09 14:51:08 +1200

    added warning if data missing from leaderboards. Fix for server being down :/

---
+++ src/lib/dataMissingWarning.svelte
@@ -0,0 +1,35 @@
+<main>
+  <div class="missing-data-warning">
+    Data for this date is missing or incomplete. This could be because the user was inactive, or because data was not gathered properly on that date.  
+    <br><br>
+    If data was not gathered, the information shown may be incorrect or partially missing.
+  </div>
+
+</main>
+
+<style>
+  .missing-data-warning {
+    font-family: sans-serif;
+    padding: 15px;
+    background-color: rgb(160, 0, 0);
+    border-radius: 10px;
+    text-align: center;
+    font-size: 15px;
+    width: 1fr;
+    color: white;
+    font-weight: bolder;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+    display: flex;
+    flex-direction: column;
+    justify-content: baseline;
+    align-items: baseline;
+    transition: 0.3s;
+    margin-bottom: 15px;
+  }
+
+  .missing-data-warning:hover {
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+    background-color: rgb(175, 0, 0);
+    transition: 0.3s;
+  }
+</style>
