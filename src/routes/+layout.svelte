1eb0cb656cd27e60ab49e5f9e0f86b5fe9a406bb
Ai-Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2025-11-06 20:38:19 +1300

    added map star history, improved background, test anime girls on sides, made logo go home, added some animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
     import { onMount } from 'svelte';
     import { page } from '$app/state';
     import { fade } from 'svelte/transition';
+
     let { children }  = $props();
 
     onMount(() => {
@@ -29,40 +30,122 @@
         {@render children()}
     </span>
 
+
+  <div class="anime-girl-area" class:moved={(scroll_y <= 25)}>
+    <!--<img src="/left.png" alt="" class="anime-girl right-anime-girl">
+    <img src="/right.png" alt="" class="anime-girl left-anime-girl">-->
+  </div>
+
+
     <Footer />
 </main>
 {:else}
   {@render children()}
 {/if}
   
-  
-<svelte:window bind:scrollY={scroll_y} />
-  
-  <style>  
 
+<svelte:body class:compact={compact} />
+<svelte:window bind:scrollY={scroll_y} />
+
+<style>  
+
+  .anime-girl-area {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+    align-items: flex-start;
+    transform: translateY(0);
+    transition: transform 1.0s ease;
+  }
+
+  .anime-girl-area.moved {
+    transform: translateY(40px);
+  }
+
+
+  .anime-girl {
+    visibility: hidden;
+    max-width: calc(((100% - 1000px) / 2)); 
+    height: 1fr;
+    padding: 50px;
+    max-height: calc(100vh - 20%);
+  }
+
+  .left-anime-girl {
+    animation: float 3s ease-in-out infinite;
+    align-self: flex-start;
+    padding-bottom: 20%;
+    filter: drop-shadow(0px 0px 15px rgba(160, 20, 20, 0.5));
+  }
+  
+  .right-anime-girl {
+    animation: float 3s ease-in-out infinite;
+    animation-delay: -0.5s;
+    align-self: flex-end;
+    filter: drop-shadow(0px 0px 15px rgba(20, 90, 160, 0.5));
+  }
+
+    @keyframes float {
+      0% {
+          transform: translateY(0);
+      }
+
+      50% {
+          transform: translateY(-2%);
+      }
+
+      100% {
+          transform: translateY(0);
+      }
+  }
+
+  .hero {
+    padding: 25px 25px;
+    background-color: rgba(0, 0, 0, 0.55);
+    border-radius: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+    margin-top: 90px;
+    max-width: 900px;
+    width: 90%;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+  }
+
+  @media (min-width: 1500px) {
+    .anime-girl {
+      visibility: visible;
+      max-width: calc(((100% - 900px) / 2)); 
+    }
+  }
+
+  @media (min-width: 1700px) {
     .hero {
-      padding: 25px 25px;
-      background-color: rgba(0, 0, 0, 0.55);
-      border-radius: 15px;
-      margin-left: auto;
-      margin-right: auto;
-      margin-bottom: 20px;
-      margin-top: 90px;
-      max-width: 1000px;
-      width: 90%;
-      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+      max-width: 1100px;
     }
-  
-    .layout {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;  /* Ensures the layout takes the full height of the screen */
+
+    .anime-girl {
+      max-width: calc(((100% - 1000px) / 2)); 
     }
-  
-    main {
-      font-family: "Inter", sans-serif;
-      color: white;
-      flex: 1;  /* Takes up remaining space */
-      min-height: calc(100vh - 70px);
-    }
-  </style>
+  }
+
+  .layout {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;  /* Ensures the layout takes the full height of the screen */
+  }
+
+  main {
+    font-family: "Inter", sans-serif;
+    color: white;
+    flex: 1;  /* Takes up remaining space */
+    min-height: calc(100vh - 70px);
+  }
+</style>
