88af3381f6bdfe36c77b5b4dd52968ec63045b4e
Ai-Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2026-02-09 18:50:50 +1300

    improved footer. Changed lisence

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -15,9 +15,11 @@
     </div>
 
     <div class="credit-row">
-      © 2025 HistorySaber. <br>
-      Licensed under the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License, Version 2.0</a>.
+      © 2025-2026 HistorySaber. <br>
     </div>
+  </div>
+  <div class="footer-cubes">
+    <img src="/images/footer-cubes.avif" alt="">
   </div>
 </footer>
   
@@ -28,20 +30,26 @@
       z-index: 10;
       background-color: rgb(0, 0, 0);
       position: relative;
+      display: flex;
+      flex-direction: row;
+      justify-content: stretch;
+      align-items: stretch;
     }
     .text-sections {
       font-family: "Inter", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
       color: rgb(230,230,230);
       display: flex;
       justify-content: start;
-      align-items: start;
+      align-items: center;
       flex-direction: column;
+      font-size: 1vw;
       padding: 16px;
       gap: 2px;
+      flex: 1.5;
     }
     
     .small-logo {
-      width: 350px;
+      width: 30vw;
     }
 
     .resources-row {
@@ -56,5 +64,19 @@
     a {
       color: rgb(200, 200, 200);
       text-decoration: none;
+    }
+
+    .footer-cubes {
+      display: flex;
+      flex-direction: column;
+      align-items: end;
+      right: 0px;
+      flex: 1;
+    }
+    .footer-cubes img {
+      display: block;
+      flex: 1 1 0;
+      height: 100%;
+      object-fit: contain; /* or cover */
     }
   </style>
